7c3cd29bc465c1b388daf8da718e49e010e940dd
Tom Lo <loto2@oocl.com>
2020-11-30 18:44:12 +0800

    [word-frequency-game][Tom] refactor: rename test names to use format should_when_given

--- src/test/java/com/oocl/wordfrequency/WordFrequencyGameTest.java
+++ src/test/java/com/oocl/wordfrequency/WordFrequencyGameTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class WordFrequencyGameTest {
 
     @Test
-    public void should_get_the_1_when_input_the() {
+    public void should_get_the_1_when_get_result_given_the() {
         //Given
         String inputStr = "the";
         String expectResult = "the 1";
@@ -15,7 +15,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_two_words() {
+    public void should_return_the_1_is_1_when_get_result_given_the_is() {
         //Given
         String inputStr = "the is";
         String expectResult = "the 1\nis 1";
@@ -23,7 +23,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_two_words_with_special_spaces() {
+    public void should_return_the_1_is_1_when_get_result_given_the_is_with_special_spaces() {
         //Given
         String inputStr = "the      is";
         String expectResult = "the 1\nis 1";
@@ -31,7 +31,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_two_words_with_special_enter() {
+    public void should_return_the_1_is_1_when_get_result_given_the_is_with_special_enter() {
         //Given
         String inputStr = "the   \n   is";
         String expectResult = "the 1\nis 1";
@@ -39,7 +39,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_pracess_two_same_words_with_sorted() {
+    public void should_return_the_2_is_1_when_get_result_given_the_the_is() {
         //Given
         String inputStr = "the the is";
         String expectResult = "the 2\nis 1";
@@ -47,7 +47,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_sorted_with_count_descending() {
+    public void should_return_is_2_the_1_which_sort_with_count_descending_when_get_result_given_the_is_is() {
         //Given
         String inputStr = "the is is";
         String expectResult = "is 2\nthe 1";
