fa0b60d2d4bd734dae8de71667c499d9ad4a541a
Tom Lo <loto2@oocl.com>
2020-11-30 18:52:31 +0800

    [word-frequency-game][Tom] refactor: test case use given when then structure

--- src/test/java/com/oocl/wordfrequency/WordFrequencyGameTest.java
+++ src/test/java/com/oocl/wordfrequency/WordFrequencyGameTest.java
@@ -10,48 +10,72 @@ class WordFrequencyGameTest {
     void should_return_the_1_when_get_result_given_sentence_the() {
         //Given
         String sentence = "the";
-        String expected = "the 1";
-        validate_Input_words_process_to_expected_word(sentence, expected);
+
+        //When
+        String actual = new WordFrequencyGame().getResult(sentence);
+
+        //Then
+        assertEquals("the 1", actual);
     }
 
     @Test
     void should_return_the_1_is_1_when_get_result_given_the_is() {
         //Given
         String sentence = "the is";
-        String expected = "the 1\nis 1";
-        validate_Input_words_process_to_expected_word(sentence, expected);
+
+        //When
+        String actual = new WordFrequencyGame().getResult(sentence);
+
+        //Then
+        assertEquals("the 1\nis 1", actual);
     }
 
     @Test
     void should_return_the_1_is_1_when_get_result_given_the_is_with_special_spaces() {
         //Given
+
         String sentence = "the      is";
-        String expected = "the 1\nis 1";
-        validate_Input_words_process_to_expected_word(sentence, expected);
+        //When
+        String actual = new WordFrequencyGame().getResult(sentence);
+
+        //Then
+        assertEquals("the 1\nis 1", actual);
     }
 
     @Test
     void should_return_the_1_is_1_when_get_result_given_the_is_with_special_enter() {
         //Given
         String sentence = "the   \n   is";
-        String expected = "the 1\nis 1";
-        validate_Input_words_process_to_expected_word(sentence, expected);
+
+        //When
+        String actual = new WordFrequencyGame().getResult(sentence);
+
+        //Then
+        assertEquals("the 1\nis 1", actual);
     }
 
     @Test
     void should_return_the_2_is_1_when_get_result_given_the_the_is() {
         //Given
         String sentence = "the the is";
-        String expected = "the 2\nis 1";
-        validate_Input_words_process_to_expected_word(sentence, expected);
+
+        //When
+        String actual = new WordFrequencyGame().getResult(sentence);
+
+        //Then
+        assertEquals("the 2\nis 1", actual);
     }
 
     @Test
     void should_return_is_2_the_1_which_sort_with_count_descending_when_get_result_given_the_is_is() {
         //Given
         String sentence = "the is is";
-        String expected = "is 2\nthe 1";
-        validate_Input_words_process_to_expected_word(sentence, expected);
+
+        //When
+        String actual = new WordFrequencyGame().getResult(sentence);
+
+        //Then
+        assertEquals("is 2\nthe 1", actual);
     }
 
     private void validate_Input_words_process_to_expected_word(String sentence, String expected) {
