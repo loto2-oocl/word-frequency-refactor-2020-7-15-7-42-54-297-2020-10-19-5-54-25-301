5d73e27f366f6604dbabb632763f064fa6b93168
Tom Lo <loto2@oocl.com>
2020-11-30 18:36:05 +0800

    [word-frequency-game][Tom] refactor: rename result to actual

--- src/test/java/com/oocl/wordfrequency/WordFrequencyGameTest.java
+++ src/test/java/com/oocl/wordfrequency/WordFrequencyGameTest.java
@@ -57,8 +57,8 @@ public class WordFrequencyGameTest {
     private void validate_Input_words_process_to_expected_word(String inputStr, String expectResult) {
         WordFrequencyGame game = new WordFrequencyGame();
         //When
-        String result = game.getResult(inputStr);
+        String actual = game.getResult(inputStr);
         //Then
-        assertEquals(result, expectResult);
+        assertEquals(actual, expectResult);
     }
 }
